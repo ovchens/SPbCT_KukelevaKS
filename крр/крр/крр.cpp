ab729c4dd17e029886122d0627a469d7841ca0bd
ovchens <kukelevakarina@gmail.com>
2020-11-05 21:41:22 +0300

    Add kr

---
+++ крр/крр/крр.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+//#include <string>
+int main(int argc, char* argv[])
+{
+    FILE* ptrFile = fopen(argv[1], "r");
+    int c= fgetc(ptrFile);
+    std::string s = "";
+
+    while (c != EOF) // пока не конец файла
+    {
+        s += c;
+        s += ' ';
+        c = fgetc(ptrFile);    
+    } 
+
+    fclose(ptrFile);
+    std::cout << "\"" << s << "\"";
+
+    return 0;
+}
+
